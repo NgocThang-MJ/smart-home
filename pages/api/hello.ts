5f5ea4f7d2755584b3ee6e83a529f5cbad713606
Tran Ngoc Thang <tranngocthang.bkdn@gmail.com>
2022-02-21 20:36:22 +0700

    Temporary comment in cors

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -11,14 +11,14 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  await NextCors(req, res, {
-    method: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
-    origin: "*",
-  });
+  //await NextCors(req, res, {
+  //method: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+  //origin: "*",
+  //});
 
-  const { db } = await connectToDatabase();
+  //const { db } = await connectToDatabase();
 
-  const kitchen = await db.collection("kitchen").find({}).toArray();
-  console.log(kitchen);
+  //const kitchen = await db.collection("kitchen").find({}).toArray();
+  //console.log(kitchen);
   res.status(200).json({ name: "John Doe" });
 }
